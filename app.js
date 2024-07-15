f4763a080b28575bc099f2319a49e8b34e2f6d51
Muhammad Ghufran <ghufranbakrie@gmail.com>
2024-07-15 16:53:37 +0700

    upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const employeeRoutes = require('./routes/employee');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 'https://dinassosial.zeabur.app/';
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
