7dba65da5ad04b2ffe7693febd37ab2d9c8d106f
Muhammad Ghufran <ghufranbakrie@gmail.com>
2024-07-16 00:10:16 +0700

    update json

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,12 +5,13 @@ const employeeRoutes = require('./routes/employee');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 const db = mongoose.connection;
