da077f93a537d7d2a22bed0b808d3a4b5246373b
Muhammad Ghufran <ghufranbakrie@gmail.com>
2024-07-18 12:48:50 +0700

    upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const employeeRoutes = require('./routes/employeeRoutes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Verifikasi MONGO_URI
 console.log('Mongo URI:', process.env.MONGO_URI);
