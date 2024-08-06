f442e06e0b7216249f417cc4ab7423c7343ed32e
Muhammad Ghufran <ghufranbakrie@gmail.com>
2024-08-06 12:36:12 +0700

    update CUD

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const employeeRoutes = require('./routes/employeeRoutes');
 const authRoutes = require('./routes/authRoutes');
+const imageUpload = require('./routes/imageRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI);
 app.use(cors({
     origin: 'http://localhost:5173', // Ganti dengan URL frontend Anda
     credentials: true
-  }));
+}));
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error('Connection error:', error));
@@ -27,6 +28,7 @@ app.use(bodyParser.json());
 
 app.use('/employees', employeeRoutes);
 app.use('/auth', authRoutes);
+app.use('/profile', imageUpload);
 
 app.post('/', (req, res) => {
     res.status(200).send('Root Endpoint POST Request');
--- BackEnd/routes/employeeRoutes.js
+++ BackEnd/routes/employeeRoutes.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { requestEmployeeUpdate, getAllUpdateRequests, updateRequestStatus } = require('../controllers/updateRequestController');
-const { createEmployee, getAllEmployees, getEmployeeByNip, updateEmployeeByNip, deleteEmployeeByNip, importEmployeesFromCsv, getUserById } = require('../controllers/employeeController');
-const { uploadPhoto } = require('../controllers/photoController');
+const employee = require('../controllers/employeeController');
 const { authenticateToken, authorizeRoles, authorizeEmployeeAccess } = require('../middleware/authMiddleware');
 const { uploadCsv } = require('../utils/uploadUtils');
 const upload = require('../utils/multer-config');
@@ -11,25 +10,22 @@ const upload = require('../utils/multer-config');
 
 // Routes
 // Buat Karyawan Baru (Admin only)
-router.post('/', authenticateToken, authorizeRoles('admin'), createEmployee);
+router.post('/', authenticateToken, authorizeRoles('admin'), employee.createEmployee);
 
 // Get all employees (Admin only)
-router.get('/', authenticateToken, authorizeRoles('admin'), getAllEmployees);
+router.get('/', authenticateToken, authorizeRoles('admin'), employee.getAllEmployees);
 
-// Get employee by NIP (Employee can only get their own data)
-router.get('/:nip', authenticateToken, authorizeEmployeeAccess, getEmployeeByNip);
+// Get employee by NIP (only admin)
+router.get('/:nip', authenticateToken, authorizeEmployeeAccess, employee.getEmployeeByNip);
 
 // Update employee by NIP
-router.patch('/:nip', authenticateToken, authorizeEmployeeAccess, updateEmployeeByNip);
+router.patch('/:nip', authenticateToken, authorizeEmployeeAccess, employee.updateEmployeeByNip);
 
 // Delete employee by NIP
-router.delete('/:nip', authenticateToken, authorizeRoles('admin'), deleteEmployeeByNip);
+router.delete('/:nip', authenticateToken, authorizeRoles('admin'), employee.deleteEmployeeByNip);
 
 // import employees from CSV (Admin only)
-router.post('/import', authenticateToken, authorizeRoles('admin'), uploadCsv, importEmployeesFromCsv);
-
-// Upload photo
-router.post('/upload-foto', authenticateToken, authorizeRoles('admin'), upload.single('image'), uploadPhoto);
+router.post('/import', authenticateToken, authorizeRoles('admin'), uploadCsv, employee.importEmployeesFromCsv);
 
 // Request update data
 router.post('/update-request', authenticateToken, requestEmployeeUpdate);
@@ -41,6 +37,6 @@ router.get('/request/update-requests', authenticateToken, authorizeRoles('admin'
 router.patch('/update-request/:id', authenticateToken, authorizeRoles('admin'), updateRequestStatus);
 
 // Get user data by ID (self-access only)
-router.get('/:id', authenticateToken, getUserById);
+router.get('/:id', authenticateToken, employee.getUserById);
 
 module.exports = router;
