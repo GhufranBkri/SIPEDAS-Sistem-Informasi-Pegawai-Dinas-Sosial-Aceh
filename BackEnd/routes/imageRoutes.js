077d027e845ce083da5a051beff58bb65056041c
Muhammad Ghufran <ghufranbakrie@gmail.com>
2024-08-07 11:55:44 +0700

    fix upload image

--- BackEnd/routes/imageRoutes.js
+++ BackEnd/routes/imageRoutes.js
@@ -5,12 +5,12 @@ const upload = require('../utils/multer-config');
 const photoController = require('../controllers/photoController');
 
 // Menggunakan middleware multer di endpoint yang sesuai
-router.post('/upload-foto', authenticateToken, authorizeRoles('admin'), upload.single('file'), photoController.uploadPhoto);
+router.post('/upload-foto', authenticateToken, authorizeRoles('admin'), upload.single('image'), photoController.uploadPhoto);
 
 // Menggunakan middleware authenticateToken dan authorizeRoles di endpoint yang sesuai
-router.put('/edit-foto', authenticateToken, authorizeRoles('admin'), upload.single('file'), photoController.editPhoto);
+router.put('/edit-foto', authenticateToken, authorizeRoles('admin'), upload.single('image'), photoController.editPhoto);
 
 // Menggunakan middleware authenticateToken dan authorizeRoles di endpoint yang sesuai
-router.delete('/delete-foto', photoController.deletePhoto);
+router.delete('/delete-foto', authenticateToken, authorizeRoles('admin'), upload.single('image'), photoController.deletePhoto);
 
 module.exports = router;
