e66ef8166763cbda58a2dee9105fa35ca62ad904
Muhammad Ghufran <ghufranbakrie@gmail.com>
2024-08-15 23:10:54 +0700

    update route for request data

--- BackEnd/routes/requestRoutes.js
+++ BackEnd/routes/requestRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { requestEmployeeUpdate, getAllUpdateRequests, updateRequestStatus, getPendingUpdateRequests } = require('../controllers/updateRequestController');
+const { requestEmployeeUpdate, getAllUpdateRequests, updateRequestStatus, getPendingUpdateRequests, getUpdateRequestById } = require('../controllers/updateRequestController');
 const { authenticateToken, authorizeRoles, authorizeEmployeeAccess } = require('../middleware/authMiddleware');
 
 // Request update data
@@ -9,8 +9,13 @@ router.post('/update-request', authenticateToken, requestEmployeeUpdate);
 // Get all update requests (Admin only)
 router.get('/update-request', authenticateToken, authorizeRoles('admin'), getAllUpdateRequests);
 
+// Get requests by id (Admin only)
+router.get('/update-request/:id', authenticateToken, authorizeRoles('admin'), getUpdateRequestById);
+
 // Get all update requests (Admin only)
 router.get('/pending-request', authenticateToken, authorizeRoles('admin'), getPendingUpdateRequests);
+
+
 
 // Endpoint untuk menyetujui atau menolak permintaan pembaruan
 router.put('/update-request/:id', authenticateToken, authorizeRoles('admin'), updateRequestStatus);
