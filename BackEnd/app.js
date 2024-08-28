0ca1ec69b04a22d4c30c332ed87e57342c2e4b22
GhufranBkri <ghufranbakrie@gmail.com>
2024-08-28 23:11:01 +0700

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -44,6 +44,12 @@ app.use((req, res, next) => {
     res.status(404).send('Not Found');
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
