93c87d00a91fdb785a57e65eeee5769e1663ab67
GhufranBkri <ghufranbakrie@gmail.com>
2024-08-29 02:23:30 +0700

    Update Navbar.jsx

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <a href="/" className="inline w-52 mr-2">
-            <img src="src/assets/logo_text.svg" />
+            <img src="https://res.cloudinary.com/dn6yazesz/image/upload/v1724872950/logo_text_cm6nvj.svg" />
           </a>
         </div>
         <a href="/login">
