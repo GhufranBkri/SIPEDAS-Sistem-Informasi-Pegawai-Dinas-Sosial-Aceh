e41b3bbea6ec993097f492f5a08f4356eaa6164c
GhufranBkri <ghufranbakrie@gmail.com>
2024-08-29 17:55:18 +0700

    updaye app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 
 console.log('Mongo URI:', process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => {
         console.error('Failed to connect to MongoDB', err);
