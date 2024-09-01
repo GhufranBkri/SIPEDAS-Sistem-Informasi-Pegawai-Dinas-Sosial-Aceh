9eac62bee2ade16bbd37e38fdffc36a8eefa81a6
GhufranBkri <ghufranbakrie@gmail.com>
2024-09-01 20:29:37 +0700

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -30,6 +30,7 @@ app.use(cors({
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
+            console.error(`Blocked by CORS: ${origin}`);
             callback(new Error('Not allowed by CORS'));
         }
     },
@@ -37,9 +38,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
-
-// Respond to OPTIONS requests for preflight checks
-app.options('*', cors());
 
 // Body parser
 app.use(bodyParser.json());
